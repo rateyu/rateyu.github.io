507f80ffa480ef52f3c3be2b015fabb2f717bc63
rateyu <rateyu@gmail.com>
2016-01-10 18:32:57 +0800

    shell脚本命令 demo

---
+++ myshell/test.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+today=`date --date='0 days ago' +%Y%m%d`
+echo "$today"
+
+for var in 10 20 ; do
+    echo "$var"
+    if [ ! -d /wind/ ]
+    then 
+       echo "no wind floder"
+    fi
+
+    if [ -d /root/ ]
+    then 
+        echo "has root floder"
+    fi
+
+done
