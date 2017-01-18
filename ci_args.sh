3ea9e45b55e83e629733e21670ea398669f87626
rateyu <rateyu@gmail.com>
2017-01-18 22:30:43 +0800

    sed删除validate

--- ci_args.sh
+++ ci_args.sh
@@ -2,10 +2,12 @@ echo "******************************"
 echo "需要在emacs中 org导出为html文件"
 echo "******************************"
 
-sh mv_html2post.sh
-echo "copy files md and html"
 sed -i '/24.5.1/'d ./orgs/*.html
 sed -i '/Validate/'d ./orgs/*.html
+
+
+sh mv_html2post.sh
+echo "copy files md and html"
 git add -A
 git status
 git commit -m "$1"
