63554c2f2969ce22ab01b86ca9301675077a3b28
rateyu <rateyu@gmail.com>
2017-01-18 22:25:59 +0800

    sed删除validate

--- ci_args.sh
+++ ci_args.sh
@@ -5,7 +5,7 @@ echo "******************************"
 sh mv_html2post.sh
 echo "copy files md and html"
 sed -i '/24.5.1/'d ./orgs/*.html
-
+sed -i '/Validate/'d ./orgs/*.html
 git add -A
 git status
 git commit -m "$1"
