bdd82c95aad44c7aaea3c4ce16b240664f9113f7
rateyu <rateyu@gmail.com>
2017-03-14 12:39:31 -0400

    smtp_add ssl

--- urltextmail/smtp.py
+++ urltextmail/smtp.py
@@ -17,7 +17,13 @@ smtp_server = 'smtp.163.com'
 
 from email.mime.text import MIMEText
 import smtplib
-server = smtplib.SMTP(smtp_server, 25) # SMTP协议默认端口是25
+#server = smtplib.SMTP(smtp_server, 25) # SMTP协议默认端口是25
+server = smtplib.SMTP()
+server.connect('smtp.163.com')
+server.ehlo()
+server.starttls()
+server.ehlo()
+
 server.set_debuglevel(1)
 server.login(from_addr, password)
 
