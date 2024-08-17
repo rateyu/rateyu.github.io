3011fc1d93fa505080a64df95d6b42c4665c16a3
rateming <52134909@qq.com>
2024-08-18 07:13:36 +0800

    优化emacs中 图片截图与显示比例设置 优化eshell结合emacs书签快捷访问linux服务器的shell命令行

--- mv_html2post.sh
+++ mv_html2post.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
-
 cp ./orgs/*.md ./_posts/
+cd ./orgs/
+find ./ -type f -name '*.html' -exec sed -i '' 's/emacs_magit,org-mod/\/emacs_magit,org-mod/g' {} +
+cd ../
 mv ./orgs/*.html ./_posts/
 cp ./orgs/emacs_magit,org-mod/* ./emacs_magit,org-mod/
 
